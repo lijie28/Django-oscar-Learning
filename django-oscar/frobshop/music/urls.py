a76e0fb945f9abaa15c36aa75bc9f85f301e246a
jacklee <lijie9228@hotmail.com>
2017-02-23 12:21:52 +0800

    30 课

--- django-oscar/frobshop/music/urls.py
+++ django-oscar/frobshop/music/urls.py
@@ -6,11 +6,17 @@ app_name = 'music'
 
 urlpatterns = [
 
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     # music/<album_id>/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
-    # music/<album_id>/favourite
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite')
+
+
+
+    # url(r'^$', views.index, name='index'),
+    # # music/<album_id>/
+    # url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    # # music/<album_id>/favourite
+    # url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite')
 ]
 
